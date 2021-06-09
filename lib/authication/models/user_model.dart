0ffe4fc1e346333d0429dc55ff6ed76f1cd9d255
timurkarev <timurkarev@mail.ru>
2021-06-09 16:51:01 +0300

    auth in prgr

---
+++ lib/authication/models/user_model.dart
@@ -0,0 +1,6 @@
+class UserModel {
+  String email = '';
+  String name = 'Незнакомец';
+  String uid = '';
+  Set<String> roles = {'not_assigned'};
+}
---
+++ lib/authication/services/firebase_auth.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authProvider =
+    Provider<FirebaseAuthService>((ref) => FirebaseAuthService());
+
+class FirebaseAuthService {
+  final _auth = FirebaseAuth.instance;
+
+  Stream<User?> authStateChanges() => _auth.userChanges();
+
+  Future<User?> signInAnonymously() async {
+    final userCredential = await _auth.signInAnonymously();
+    return userCredential.user;
+  }
+
+  Future<User?> signInEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      final userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential.user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future<UserCredential> createUserWithEmailAndPassword(
+      {required String email, required String password}) async {
+    final userCredential = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    return userCredential;
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
