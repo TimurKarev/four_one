36096d260c718856897a7783ab769cd3cb708469
timurkarev <timurkarev@mail.ru>
2021-06-16 14:59:56 +0300

    design fixes

--- lib/four_one/utils/formatters.dart
+++ lib/four_one/utils/formatters.dart
@@ -1,5 +1,8 @@
+import 'package:intl/intl.dart';
+
 String formatDate(DateTime dt) {
-  return '${dt.day}.${dt.month}.${dt.year}';
+  //return '${dt.day}.${dt.month}.${dt.year}';
+  return DateFormat("dd.MM.yy").format(dt);
 }
 
 String getNumFromFormatString(String str) {
